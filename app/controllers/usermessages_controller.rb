b2bb8b4f60ce2114e0bc6d19565b29876ae65d3c
Ramesh Kumar Subramaniam <asp.ramesh@gmail.com>
2017-02-22 17:18:55 +1100

    Completed Contact Form

--- app/controllers/usermessages_controller.rb
+++ app/controllers/usermessages_controller.rb
@@ -4,10 +4,14 @@ class UsermessagesController < ApplicationController
 	end
 	def create
 	  @usermessage = Usermessage.new(usermessages_params)
-	  if @usermessage.save
-	     redirect_to new_usermessage_path, notice: "Message sent."
-	  else
-	     redirect_to new_usermessage_path, notice: "Error occured."
+	  if @usermessage.save	  		
+	  		flash[:success] = "Message sent."
+	     	redirect_to new_usermessage_path
+	  else	  	 
+  	  @usermessage.errors.each do |attr, message| 
+     		flash[:danger] = flash[:danger].to_s + "\n" + message 
+      end 
+	    redirect_to new_usermessage_path
 	  end
 	end
 	private
--- app/models/usermessage.rb
+++ app/models/usermessage.rb
@@ -1,4 +1,9 @@
 class Usermessage < ActiveRecord::Base	
+	validates :First_Name, :presence => { :message => "First Name is required." }
+	validates :Last_Name, :presence => { :message => "Last Name is required." }
+  validates :Email, :presence => { :message => "Email is required." }
+  validates :Comments, :presence => { :message => "Comments is required." }
+
 	before_save :default_values
 
   def default_values   	
